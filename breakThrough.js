dcb1117d521033758c79da8f85e863897450aaa5
ashishgupta8080 <ag.ashishgupta011@gmail.com>
2024-09-24 11:15:07 +0530

    check1

--- breakThrough.js
+++ breakThrough.js
@@ -16,37 +16,70 @@ function createAnimations() {
             trigger: ".section2-parent",
             start: "top top",
             end: "bottom top",
-            toggleActions: "restart pause resume none",
-            // scrub: true, 
+            toggleActions: "restart pause resume reset",
+            scrub: 1,
             pin: true,
             anticipatePin: 1,
-            markers: false, 
-            invalidateOnRefresh: true
+            // markers: true
         }
     });
 
     const mm = gsap.matchMedia();
 
     mm.add("(max-width: 425px)", () => {
-        tl.to(".eyva-img", { y: -650, scale: 0.9, duration: 3 }) 
-          .to(".breakThrough-txt", { opacity: 0, duration: 1.5 })
+        tl.to(".eyva-img", { y: -650, scale: 0.9, duration: 5 })
+          .to(".breakThrough-txt", { opacity: 0, duration: 1.8 })
           .fromTo(".break-through-content", { opacity: 0, y: 0 }, { opacity: 1, y: 0, duration: 1 });
     });
 
     mm.add("(min-width: 426px) and (max-width: 767px)", () => {
-        tl.to(".eyva-img", { y: -700, scale: 0.9, duration: 3 }) 
-          .to(".breakThrough-txt", { opacity: 0, duration: 1.5 }, "-=1.2")
-          .fromTo(".break-through-content", { opacity: 0, y: 0 }, { opacity: 1, y: 0, duration: 1, ease: "power2.out" }, "-=1.2");
+        tl.to(".eyva-img", {
+            y: -700,
+            scale: 0.9,
+            ease: "power2.out",
+            duration: 5,
+        });
+
+        tl.to(".breakThrough-txt", {
+            opacity: 0,
+            duration: 1.8,
+        }, "-=1.2");
+
+        tl.fromTo(".break-through-content",
+            { opacity: 0, y: 0 },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 1,
+                ease: "power2.out",
+            }, "-=1.2");
     });
 
     mm.add("(min-width: 768px)", () => {
-        tl.to(".eyva-img", { y: -700, scale: 0.8, duration: 2 }) 
-          .to(".breakThrough-txt", { opacity: 0, duration: 1 }, "-=1")
-          .fromTo(".break-through-content", { opacity: 0, y: 50 }, { opacity: 1, y: 0, duration: 2, ease: "power2.out" }, "-=1");
+        tl.to(".eyva-img", {
+            y: -700,
+            scale: 0.8,
+            ease: "power2.out",
+            duration: 3,
+        });
+
+        tl.to(".breakThrough-txt", {
+            opacity: 0,
+            duration: 1.5,
+        }, "-=1");
+
+        tl.fromTo(".break-through-content",
+            { opacity: 0, y: 50 },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 3,
+                ease: "power2.out",
+            }, "-=1");
     });
 
     ScrollTrigger.refresh();
 }
 
 createAnimations();
-window.addEventListener('resize', debounce(createAnimations, 200)); 
+window.addEventListener('resize', debounce(createAnimations, 200));
